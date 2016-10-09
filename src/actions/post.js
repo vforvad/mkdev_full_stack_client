34f21df96d236e0e6d73878c02d79b74cc6f871c
Vadim Sokoltsov <vforvad@gmail.com>
2016-10-10 00:59:50 +0300

    implement creating of post

--- src/actions/post.js
+++ src/actions/post.js
@@ -1,8 +1,18 @@
-import { get } from 'utils/ApiRequest';
+import { get, post } from 'utils/ApiRequest';
 
 export function getPosts() {
   return get('/posts')
             .then((response) => {
               return response.data.posts;
             });
+}
+
+export function createPost(postData) {
+  return post('/posts', { post: postData })
+    .then((response) => {
+      return response.data.post;
+    })
+    .catch((errors) => {
+      return erros.data.errors;
+    });
 }
--- src/components/Post/Form/PostForm.jsx
+++ src/components/Post/Form/PostForm.jsx
@@ -1,10 +1,51 @@
 import React, { Component } from 'react';
 
+import { createPost } from 'actions/post';
+
 export default class PostForm extends Component {
 
+  state = {
+    title: '',
+    body: '',
+    username: ''
+  };
+
+  onChange(event) {
+    let prevState = this.state;
+
+    prevState[event.target.name] = event.target.value;
+    this.setState(prevState);
+  }
+
+  submitForm() {
+    createPost(this.state)
+      .then((post) => {
+        alert('Post created');
+      })
+      .catch((errors) => {
+        alert('An error has occured');
+      });
+  }
+
   render() {
+    const { title, body, username } = this.state;
+
     return (
-      <div>Добавление записи запрещено</div>
+      <div>
+        <div>
+          <input type="text" name="username" value={username}
+            onChange={this::this.onChange} placeholder="Username" />
+        </div>
+        <div>
+          <input type="text" name="title" value={title}
+            onChange={this::this.onChange} placeholder="Title" />
+        </div>
+        <div>
+          <textarea name="body" onChange={this::this.onChange}
+            value={body} placeholder="Body" />
+        </div>
+        <button onClick={this::this.submitForm}>Save</button>
+      </div>
     );
   }
 }
